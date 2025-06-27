6d959b9156f0f667d901af782fc646ed290372a3
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2025-06-27 13:39:14 +0700

    chore(backend-service): install springboot backend geobill v.1.0

---
+++ BE/geobill/src/main/java/com/alexistdev/geobill/GeobillApplication.java
@@ -0,0 +1,13 @@
+package com.alexistdev.geobill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeobillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeobillApplication.class, args);
+	}
+
+}
---
+++ BE/geobill/src/test/java/com/alexistdev/geobill/GeobillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexistdev.geobill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeobillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
