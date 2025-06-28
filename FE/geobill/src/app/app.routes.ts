7a7f83a43fb2ace413f058d95eab3db687981b16
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2025-06-28 23:43:14 +0700

    feat(login-layout): create login and not found component, and setup image for login background logo

--- FE/geobill/src/app/app.routes.ts
+++ FE/geobill/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import {Dashboard} from './admin/dashboard/dashboard';
 import {Notfoundcomponent} from './share/notfoundcomponent/notfoundcomponent';
+import {Login} from './login/login';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  {
+    path: 'login',
+    component: Login
+  },
   {
     path: 'admin/dashboard',
     component: Dashboard
---
+++ FE/geobill/src/app/login/login.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+
+}
