011dd41e6e7a287dfe6d9efce135128033475dcd
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2025-06-29 11:41:19 +0700

    feat(localstorage-service): implement Localstorageservice for sessionStorage management
    - Add methods for setting, getting, and clearing sessionStorage items.
    - Include encoding and decoding utilities to handle special characters.
    - Ensure values stored in sessionStorage are encoded for safety.

---
+++ FE/geobill/src/app/utils/localstorage/localstorageservice.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Localstorageservice {
+
+  constructor() { }
+
+  setItem(key: string, val:string):void {
+    sessionStorage.setItem(key,this.encode(val));
+  }
+
+  getItem(key:string):string {
+    return <string>sessionStorage.getItem(key);
+  }
+
+  clearItem():void {
+    sessionStorage.clear();
+  }
+
+  private encode(value: string): string {
+    return btoa(
+      encodeURIComponent(value).replace(/%([0-9A-F]{2})/g, (_, p1) =>
+        String.fromCharCode(parseInt(p1, 16))
+      )
+    );
+  }
+
+  public decode(value: string): string {
+    return decodeURIComponent(
+      Array.prototype.map.call(atob(value), (c: string) =>
+        `%${c.charCodeAt(0).toString(16).padStart(2, '0')}`
+      ).join('')
+    );
+  }
+}
