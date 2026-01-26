5131b0a1bc6329fb3064a8eeaecf1533f69cd0b1
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2026-01-26 19:52:19 +0700

    feat(i18n): Add message source and locale configuration
    - Introduced `MessageSourceConfig` and `LocaleConfig` for internationalization support.
    - Added message property files for English and Indonesian languages.
    - Updated `Customer` entity with internationalized validation messages using entity constants.

--- BE/geobill/src/main/java/com/alexistdev/geobill/config/EntityConstant.java
+++ BE/geobill/src/main/java/com/alexistdev/geobill/config/EntityConstant.java
@@ -4,6 +4,7 @@ public class EntityConstant {
 
     /** Customer */
     public static final int tableLength255 = 255;
-    public static final String userRequired = "User is required";
+    public static final String userRequired = "{customer.userId.required}";
+    public static final String maxCharacter255 = "Maximum character length is 255 characters";
 
 }
--- BE/geobill/src/main/java/com/alexistdev/geobill/models/entity/Customer.java
+++ BE/geobill/src/main/java/com/alexistdev/geobill/models/entity/Customer.java
@@ -5,12 +5,12 @@ import com.alexistdev.geobill.config.EntityConstant;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-
 import java.io.Serializable;
 import java.util.UUID;
 
@@ -36,42 +36,52 @@ public class Customer implements Serializable {
     private User user;
 
     @Nullable
+    @Size(max = EntityConstant.tableLength255, message = "{customer.firstName.size}")
     @Column(length = EntityConstant.tableLength255 , name = "first_name")
     private String firstName;
 
     @Nullable
+    @Size(max = EntityConstant.tableLength255, message = "{customer.lastName.size}")
     @Column(length = EntityConstant.tableLength255 , name = "last_name")
     private String lastName;
 
     @Nullable
+    @Size(max = EntityConstant.tableLength255, message = "{customer.businessName.size}")
     @Column(length = EntityConstant.tableLength255 , name = "business_name")
     private String businessName;
 
     @Nullable
+    @Size(max = EntityConstant.tableLength255, message = "{customer.address1.size}")
     @Column(length = EntityConstant.tableLength255 , name = "address1")
     private String address1;
 
     @Nullable
+    @Size(max = EntityConstant.tableLength255, message = "{customer.address2.size}")
     @Column(length = EntityConstant.tableLength255 , name = "address2")
     private String address2;
 
     @Nullable
+    @Size(max = EntityConstant.tableLength255, message = "{customer.city.size}")
     @Column(length = EntityConstant.tableLength255 , name = "city")
     private String city;
 
     @Nullable
+    @Size(max = EntityConstant.tableLength255, message = "{customer.state.size}")
     @Column(length = EntityConstant.tableLength255 , name = "state")
     private String state;
 
     @Nullable
+    @Size(max = EntityConstant.tableLength255, message = "{customer.country.size}")
     @Column(length = EntityConstant.tableLength255 , name = "country")
     private String country;
 
     @Nullable
+    @Size(max = EntityConstant.tableLength255, message = "{customer.postCode.size}")
     @Column(length = EntityConstant.tableLength255 , name = "post_code")
     private String postCode;
 
     @Nullable
+    @Size(max = EntityConstant.tableLength255, message = "{customer.phone.size}")
     @Column(length = EntityConstant.tableLength255 , name = "phone")
     private String phone;
 
