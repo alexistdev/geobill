bfa6643d1f4d5701007e4e3614638572840cf979
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2026-01-31 11:31:51 +0700

    refactor(UserController, DatabaseSeeder): enhance user handling and menu options
    - Moved to constructor-based dependency injection for better clarity in `UserController`.
    - Added handling for empty user pages using fallback mechanism.
    - Enhanced admin menu by adding "Users" option for improved accessibility.
    - Introduced `AuthDTO` to segregate authentication data from `UserDTO`.
    - Removed `menus` and `homeURL` from `UserDTO` for streamlined data representation.

--- BE/geobill/src/main/java/com/alexistdev/geobill/controllers/UserController.java
+++ BE/geobill/src/main/java/com/alexistdev/geobill/controllers/UserController.java
@@ -4,9 +4,9 @@ import com.alexistdev.geobill.dto.ResponseData;
 import com.alexistdev.geobill.dto.UserDTO;
 import com.alexistdev.geobill.models.entity.User;
 import com.alexistdev.geobill.services.UserService;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -18,48 +18,61 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.ExecutionException;
-
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private static final String NO_USER_FOUND= "No product found";
+    private final UserService userService;
+    private final ModelMapper modelMapper;
 
-    @Autowired
-    private ModelMapper modelMapper;
+    public UserController(UserService userService, ModelMapper modelMapper) {
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+    }
 
     @GetMapping("/get_all_users")
     public ResponseEntity<ResponseData<Page<UserDTO>>> getAllUserData(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "0") @PositiveOrZero int page,
+            @RequestParam(defaultValue = "10") @PositiveOrZero int size,
             @RequestParam(defaultValue = "id") String sortBy,
             @RequestParam(defaultValue = "asc") String direction
-    ) throws ExecutionException, InterruptedException {
-
+    ){
         ResponseData<Page<UserDTO>> responseData = new ResponseData<>();
         Sort.Direction sortDirection = direction.equalsIgnoreCase("desc") ?
                 Sort.Direction.DESC : Sort.Direction.ASC;
 
         Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, sortBy));
 
-        Page<User> usersPage = userService.getAllUsers(pageable);
+        Page<User> usersPage;
 
-        responseData.getMessages().add("No users found");
-        responseData.setStatus(false);
-        if (!usersPage.isEmpty()) {
-            responseData.setStatus(true);
-            responseData.getMessages().removeFirst();
-            responseData.getMessages().add("Retrieved page " + page + " of users"
-            );
+        try {
+            usersPage = userService.getAllUsers(pageable);
+        } catch (RuntimeException e){
+            Pageable fallbackPageable = PageRequest.of(page, size, Sort.by(sortDirection, "id"));
+            usersPage = userService.getAllUsers(fallbackPageable);
         }
+
+        responseData.getMessages().add(NO_USER_FOUND);
+        responseData.setStatus(false);
+
+        handleNonEmptyPage(responseData, usersPage, page + 1);
 
         Page<UserDTO> userDTOS = usersPage
                 .map(user -> modelMapper.map(user, UserDTO.class));
-
         responseData.setPayload(userDTOS);
         return ResponseEntity.status(HttpStatus.OK).body(responseData);
+    }
+
+
+    private <T> void handleNonEmptyPage(ResponseData<Page<T>> responseData, Page<?> pageResult, int pageNumber){
+        if(!pageResult.isEmpty()){
+            responseData.setStatus(true);
+            if(!responseData.getMessages().isEmpty()){
+                responseData.getMessages().removeFirst();
+            }
+            responseData.getMessages().add("Retrieved page " + pageNumber + " of products");
+        }
     }
 }
--- BE/geobill/src/main/java/com/alexistdev/geobill/dto/AuthDTO.java
+++ BE/geobill/src/main/java/com/alexistdev/geobill/dto/AuthDTO.java
@@ -4,14 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
-public class UserDTO {
+public class AuthDTO {
     private String id;
     private String fullName;
     private String email;
     private String role;
+    private List<MenuDTO> menus;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
+    private String homeURL;
 }
