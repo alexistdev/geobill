904a309463ba690c2d5681331fdc5d1e67292e28
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2026-02-01 18:25:00 +0700

    feat(dto): add UserDetailDTO for user information handling
    - Created `UserDetailDTO` class to encapsulate user information with attributes like `id`, `fullName`, `email`, `role`, and dates (`createdDate`, `modifiedDate`).

---
+++ BE/geobill/src/main/java/com/alexistdev/geobill/dto/UserDetailDTO.java
@@ -0,0 +1,18 @@
+package com.alexistdev.geobill.dto;
+
+import com.alexistdev.geobill.models.entity.Customer;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Date;
+
+@Getter
+@Setter
+public class UserDetailDTO {
+    private String id;
+    private String fullName;
+    private String email;
+    private String role;
+    private Date createdDate;
+    private Date modifiedDate;
+    private Customer customer;
+}
