b1346a0e3e020cb3323caa3cd580239d66cc0985
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2026-02-01 15:08:50 +0700

    feat(auth): implement rate limiting in AuthController
    - Integrated Resilience4j rate limiter in `AuthController` to restrict excessive login attempts.
    - Added configuration in constructor to set rate limit parameters.
    - Updated properties files for internationalized rate limiting messages.
    - Modified login endpoint to return HTTP 429 status when rate limit is exceeded.

--- BE/geobill/src/main/java/com/alexistdev/geobill/controllers/AuthController.java
+++ BE/geobill/src/main/java/com/alexistdev/geobill/controllers/AuthController.java
@@ -22,9 +22,17 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
+
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Slf4j
 @CrossOrigin(origins = "http://localhost:4200/login")
@@ -36,6 +44,7 @@ public class AuthController {
     private final ModelMapper modelMapper;
     private final MessageSource messageSource;
     private final MenuService menuService;
+    private final RateLimiter rateLimiter;
 
     private static final Logger logger = Logger.getLogger(AuthController.class.getName());
 
@@ -44,6 +53,15 @@ public class AuthController {
         this.modelMapper = modelMapper;
         this.messageSource = messageSource;
         this.menuService = menuService;
+
+        RateLimiterConfig config = RateLimiterConfig.custom()
+                .limitRefreshPeriod(Duration.ofSeconds(60))
+                .limitForPeriod(5)
+                .timeoutDuration(Duration.ofSeconds(3))
+                .build();
+        RateLimiterRegistry registry = RateLimiterRegistry.of(config);
+        this.rateLimiter = registry.rateLimiter("rateLimiter");
+
     }
 
     @PostMapping("/register")
@@ -51,17 +69,17 @@ public class AuthController {
         ResponseData<User> responseData = new ResponseData<>();
         handleErrors(errors, responseData);
 
-        if(!responseData.isStatus()){
+        if (!responseData.isStatus()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
         }
 
         try {
-            String msgSuccess = this.messageLocale( "authcontroller.register.success");
+            String msgSuccess = this.messageLocale("authcontroller.register.success");
             responseData.setPayload(userService.registerUser(userRequest));
             responseData.getMessages().add(msgSuccess);
             responseData.setStatus(true);
             return ResponseEntity.status(HttpStatus.CREATED).body(responseData);
-        }catch (Exception e) {
+        } catch (Exception e) {
             responseData.setStatus(false);
             responseData.getMessages().add(e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
@@ -70,58 +88,70 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<ResponseData<AuthDTO>> login(@Valid @RequestBody LoginRequest loginRequest, Errors errors) {
-        ResponseData<AuthDTO> responseData = new ResponseData<>();
-        handleErrors(errors, responseData);
+        Supplier<ResponseEntity<ResponseData<AuthDTO>>> loginAttempt = () -> {
+            ResponseData<AuthDTO> responseData = new ResponseData<>();
+            handleErrors(errors, responseData);
 
+            try {
+                String msgSuccess = this.messageLocale("authcontroller.login.success");
+                responseData.getMessages().add(msgSuccess);
+                User user = userService.authenticate(loginRequest);
+                AuthDTO result = modelMapper.map(user, AuthDTO.class);
+                String role = result.getRole();
+
+                List<MenuDTO> menus = menuService.getMenusByRole(user.getRole())
+                        .stream()
+                        .map(menu -> modelMapper.map(menu, MenuDTO.class))
+                        .collect(Collectors.toList());
+
+                result.setMenus(menus);
+
+                String homeUser = "/users/dashboard";
+                result.setHomeURL(homeUser);
+
+                if (role.equals(Role.ADMIN.toString())) {
+                    String homeAdmin = "/admin/dashboard";
+                    result.setHomeURL(homeAdmin);
+                }
+
+                if (role.equals(Role.STAFF.toString())) {
+                    String homeStaff = "/staff/dashboard";
+                    result.setHomeURL(homeStaff);
+                }
+                responseData.setPayload(result);
+                responseData.setStatus(true);
+                return ResponseEntity.status(HttpStatus.OK).body(responseData);
+            } catch (SuspendedException s) {
+                logger.info(s.getMessage());
+                responseData.getMessages().removeFirst();
+                String invalidMsg = s.getMessage();
+                responseData.setStatus(false);
+                responseData.setPayload(null);
+                responseData.getMessages().add(invalidMsg);
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body(responseData);
+            } catch (Exception e) {
+                logger.info(e.getMessage());
+                responseData.setStatus(false);
+                responseData.setPayload(null);
+                responseData.getMessages().removeFirst();
+                responseData.getMessages().add(e.getMessage());
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+            }
+        };
+
+        Supplier<ResponseEntity<ResponseData<AuthDTO>>> restrictedLoginAttempt = RateLimiter.decorateSupplier(rateLimiter, loginAttempt);
         try {
-            String msgSuccess = this.messageLocale( "authcontroller.login.success");
-            responseData.getMessages().add(msgSuccess);
-            User user = userService.authenticate(loginRequest);
-            AuthDTO result =  modelMapper.map(user, AuthDTO.class);
-            String role = result.getRole();
-
-            List<MenuDTO> menus = menuService.getMenusByRole(user.getRole())
-                    .stream()
-                    .map(menu-> modelMapper.map(menu, MenuDTO.class))
-                    .collect(Collectors.toList());
-
-            result.setMenus(menus);
-
-            String homeUser = "/users/dashboard";
-            result.setHomeURL(homeUser);
-
-            if(role.equals(Role.ADMIN.toString())){
-                String homeAdmin = "/admin/dashboard";
-                result.setHomeURL(homeAdmin);
-            }
-
-            if(role.equals(Role.STAFF.toString())){
-                String homeStaff = "/staff/dashboard";
-                result.setHomeURL(homeStaff);
-            }
-            responseData.setPayload(result);
-            responseData.setStatus(true);
-            return ResponseEntity.status(HttpStatus.OK).body(responseData);
-        } catch (SuspendedException s) {
-            logger.info(s.getMessage());
-            responseData.getMessages().removeFirst();
-            String invalidMsg = s.getMessage();
+            return restrictedLoginAttempt.get();
+        } catch (io.github.resilience4j.ratelimiter.RequestNotPermitted e) {
+            String msgRatelimit = this.messageLocale("ratelimiter.restricted.message");
+            ResponseData<AuthDTO> responseData = new ResponseData<>();
             responseData.setStatus(false);
-            responseData.setPayload(null);
-            responseData.getMessages().add(invalidMsg);
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(responseData);
-        } catch (Exception e){
-            logger.info(e.getMessage());
-            responseData.setStatus(false);
-            responseData.setPayload(null);
-            responseData.getMessages().removeFirst();
-            responseData.getMessages().add(e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+            responseData.getMessages().add(msgRatelimit);
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body(responseData);
         }
-
     }
 
-    private void handleErrors(Errors errors, ResponseData<?> responseData){
+    private void handleErrors(Errors errors, ResponseData<?> responseData) {
         if (errors.hasErrors()) {
             for (ObjectError error : errors.getAllErrors()) {
                 logger.info(error.getDefaultMessage());
@@ -133,19 +163,7 @@ public class AuthController {
         }
     }
 
-    private String messageLocale(String key){
+    private String messageLocale(String key) {
         return messageSource.getMessage(key, null, LocaleContextHolder.getLocale());
-    }
-
-    private MenuDTO convertToMenuDTO(Menu menu){
-        MenuDTO menuDTO = new MenuDTO();
-        menuDTO.setId(menu.getId().toString());
-        menuDTO.setName(menu.getName());
-        menuDTO.setUrlink(menu.getUrlink());
-        menuDTO.setClasslink(menu.getClasslink());
-        menuDTO.setIcon(menu.getIcon());
-        menuDTO.setTypeMenu(menu.getTypeMenu());
-        menuDTO.setSortOrder(String.valueOf(menu.getSortOrder()));
-        return menuDTO;
     }
 }
